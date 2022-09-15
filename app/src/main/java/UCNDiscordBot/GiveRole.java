e1c556f8373fea2b8bd47ddc842789ccfc351e9a
RealSoerensen <patricklykke@live.dk>
2022-09-15 23:47:52 +0200

    Update remove and give commands
    Now takes account for roles bot can't give or remove.

--- app/src/main/java/UCNDiscordBot/GiveRole.java
+++ app/src/main/java/UCNDiscordBot/GiveRole.java
@@ -4,12 +4,18 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public class GiveRole {
     public static void giveRole(MessageReceivedEvent event, String role) {
-        // Check if the user has the role
-        if (event.getMember().getRoles().contains(event.getGuild().getRolesByName(role, true).get(0))) {
-            event.getChannel().sendMessage("You already have the role").queue();
+        // Check if bot can give the role and if the user has the role
+        if (event.getGuild().getSelfMember().canInteract(event.getGuild().getRolesByName(role, true).get(0))) {
+            if (!event.getMember().getRoles().contains(event.getGuild().getRolesByName(role, true).get(0))) {
+                event.getGuild().addRoleToMember(event.getMember(), event.getGuild().getRolesByName(role, true).get(0))
+                        .queue();
+                event.getChannel().sendMessage("You have been given the role").queue();
+            } else {
+                event.getChannel().sendMessage("You already have the role").queue();
+            }
         } else {
-            event.getGuild().addRoleToMember(event.getMember(), event.getGuild().getRolesByName(role, true).get(0))
-                    .queue();
+            event.getChannel().sendMessage("I can't give you the role").queue();
         }
+
     }
 }
